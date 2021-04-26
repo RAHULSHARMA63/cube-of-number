b75ce90892ef72d9e4d82318ee92d6d9e5e675ac
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-04-26 08:53:14 +0530

    To find the cube of the given number.

---
+++ cube of number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{ 
+   int num, cube;
+    
+   cout << "enter number \n";
+   cin >> num;
+   cube = num * num * num;
+   cout << "cube of a number is \n" << cube;     
+
+   return 0;
+
+}
+
